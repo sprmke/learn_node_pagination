ae3e1cb84cdfa8f8ebaed843fe32f8adb410857a
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-11-07 12:41:37 +0800

    342. prepagring pagination data on the server

--- controllers/shop.js
+++ controllers/shop.js
@@ -44,15 +44,28 @@ exports.getProduct = (req, res, next) => {
 
 exports.getIndex = (req, res, next) => {
   const page = req.query.page ?? 1;
+  let totalItems;
 
   Product.find()
-    .skip((+page - 1) * ITEMS_PER_PAGE)
-    .limit(ITEMS_PER_PAGE)
+    .countDocuments()
+    .then((numProducts) => {
+      totalItems = numProducts;
+
+      return Prodcut.find()
+        .skip((+page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then((products) => {
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
         path: '/',
+        totalProducts: totalItems,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PAGE_PAGE),
       });
     })
     .catch((err) => {
